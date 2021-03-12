9ac7ef7076ccf4a54a839bbb2779c5c0f0615740
Han <hanisimo@gmail.com>
2021-03-12 14:01:00 +0400

    Code optimization

---
+++ lib/constants.dart
@@ -0,0 +1,28 @@
+// App wide constants
+
+import 'package:flutter/material.dart';
+
+//App Title
+const String appTitle = 'Countdown Timer ⏳';
+
+// The circular progress indicator dimensions for web
+const double circularProgressIndicatorDimensionsForWeb = 400;
+// This edge insets padding warps the 'TimerPicker' & 'CircularProgressIndicator' stack
+const EdgeInsets edgeInsetsPadding = EdgeInsets.all(32.0);
+// The elevated buttons edge insets padding (Left & Right control buttons)
+const EdgeInsets elevatedButtonPadding = EdgeInsets.all(12.0);
+
+// Countdown duration text style
+const TextStyle countdownDurationTextStyle = TextStyle(
+    color: Colors.amberAccent, fontWeight: FontWeight.bold, fontSize: 48);
+// Elevated Button text style
+const TextStyle elevatedButtonTextStyle = TextStyle(color: Colors.white);
+
+// Circular progress indicator startup color
+const Color CircularProgressIndicatorStartupColor = Colors.blueGrey;
+// Circular progress indicator head color
+const Color CircularProgressIndicatorHeadColor = Colors.red;
+// Circular progress indicator value color
+const Color CircularProgressIndicatorValueColor = Colors.orange;
+// Circular progress indicator done (complete) color
+const Color CircularProgressIndicatorDoneColor = Colors.green;
--- lib/main.dart
+++ lib/main.dart
@@ -6,21 +6,24 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 
+import 'constants.dart';
+
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
+  // Set the preferred orientations
   SystemChrome.setPreferredOrientations([DeviceOrientation.portraitDown]);
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
+  // This widget is the root of the application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Countdown Timer ⏳',
+      title: appTitle, // Set the App title
       debugShowCheckedModeBanner: false,
-      theme: ThemeData.dark().copyWith(),
-      home: MyHomePage(title: 'Countdown Timer  ⏳'),
+      theme: ThemeData.dark(),
+      home: MyHomePage(title: appTitle), // Set the App home page title
     );
   }
 }
@@ -44,73 +47,159 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> with TickerProviderStateMixin {
-  Timer _countdownTimer;
-  TimerStatus _timerStatue = TimerStatus.set;
-  Duration _timerDuration = const Duration(hours: 0, minutes: 3, seconds: 0);
-  int _restTimerDurationInSeconds = 3 * 60;
-  String _countdownTimerAsString = '0:3:0'; // Start -> Pause -> Resume
-  String _leftControlButtonText = 'Start'; // Start -> Pause -> Resume
-  IconData _leftControlButtonIcon = Icons.play_arrow;
-  String _rightControlButtonText = 'Set Timer'; // Set Timer -> Reset
-  IconData _rightControlButtonIcon = Icons.restore;
+  Timer _countdownTimer; // Countdown Timer
+  TimerStatus _timerStatue = TimerStatus.set; // The timer's current  Status
+  Duration _timerDuration = const Duration(
+      hours: 0,
+      minutes: 3,
+      seconds:
+          0); // The Countdown timer duration (the initial/default value is 3 minutes)
+  int _restTimerDurationInSeconds =
+      3 * 60; // The rest time (till countdown to 0) duration in seconds
+  String _countdownTimerDurationAsString =
+      '0:3:0'; // The countdown timer duration as string
+  String _leftControlButtonText =
+      'Start'; // The left control button's text: 'Start' -> 'Pause' -> 'Resume'
+  IconData _leftControlButtonIcon =
+      Icons.play_arrow; // The left control button's icon (Play Arrow)
+  String _rightControlButtonText =
+      'Set Timer'; // The right control button's text: 'Set Timer'
+  IconData _rightControlButtonIcon =
+      Icons.restore; // The left control button's icon (Restore)
 
-  void startCountdownTimer({int seconds}) {
+  // Start the countdown timer
+  void _startCountdownTimer({int seconds}) {
     const oneSecondPeriod = const Duration(seconds: 1);
 
+    // Check the null safety of the "_countdownTimer", then Check if it is active!
     if ((_countdownTimer != null) && (_countdownTimer.isActive)) {
-      _countdownTimer.cancel();
+      _countdownTimer.cancel(); // Cancel/stop the countdown timer
     }
+
+    // Creates a new repeating timer
     _countdownTimer = new Timer.periodic(
       oneSecondPeriod,
       (Timer timer) {
+        // Check if the timer is already started
         if (_timerStatue == TimerStatus.started)
           setState(() {
-            _restTimerDurationInSeconds--;
-            _countdownTimerAsString = _durationAsString(
-                duration: Duration(seconds: _restTimerDurationInSeconds));
+            // Update the UI
+            _restTimerDurationInSeconds--; // Update the rest duration value
+            _countdownTimerDurationAsString = _durationAsString(
+                duration: Duration(
+                    seconds:
+                        _restTimerDurationInSeconds)); // Get the rest duration as formatted string (hh:mm:ss/00:03:00)
           });
 
+        // Check if counting down is finished (done)
         if (_restTimerDurationInSeconds == 0) {
           setState(() {
+            // Update the UI
             _timerStatue = TimerStatus.finished;
             _leftControlButtonText = 'Start';
             _leftControlButtonIcon = Icons.play_arrow;
 
-            timer.cancel();
+            timer
+                .cancel(); // Cancel the countdown timer (it is not needed anymore)
           });
         }
       },
     );
   }
 
-  // Duration As String
+  // Return the provided duration as a string hh:mm:ss/00:03:00
   String _durationAsString({Duration duration}) {
-    String twoDigits(int n) => n.toString().padLeft(2, '0');
-    String twoDigitMinutes = twoDigits(duration.inMinutes.remainder(60));
-    String twoDigitSeconds = twoDigits(duration.inSeconds.remainder(60));
-    return '${twoDigits(duration.inHours)}:$twoDigitMinutes:$twoDigitSeconds';
+    String twoDigitHours = _twoDigits(number: duration.inHours); // Hours
+    String twoDigitMinutes =
+        _twoDigits(number: duration.inMinutes.remainder(60)); // Minutes
+    String twoDigitSeconds =
+        _twoDigits(number: duration.inSeconds.remainder(60)); // Seconds
+    return '$twoDigitHours:$twoDigitMinutes:$twoDigitSeconds';
+  }
+
+  // Return two digits of provided number (as string)
+  String _twoDigits({int number}) {
+    return number.toString().padLeft(2, '0');
+  }
+
+  // On press the left control button
+  void _leftControlButtonOnPressed() {
+    // Set -> Start -> Pause -> Resume
+    switch (_timerStatue) {
+      case TimerStatus.set: // Start Now
+        _restTimerDurationInSeconds = _timerDuration.inSeconds;
+        _startCountdownTimer(seconds: _restTimerDurationInSeconds);
+        _timerStatue = TimerStatus.started;
+        setState(() {
+          _countdownTimerDurationAsString = _durationAsString(
+              duration: Duration(seconds: _restTimerDurationInSeconds));
+          _leftControlButtonText = 'Pause';
+          _leftControlButtonIcon = Icons.pause;
+        });
+        break;
+      case TimerStatus.started: // Stop Now
+        _timerStatue = TimerStatus.paused;
+
+        setState(() {
+          _leftControlButtonText = 'Resume';
+          _leftControlButtonIcon = Icons.play_arrow;
+        });
+        break;
+      case TimerStatus.paused: // Resume Now
+        // Pause the Timer Now (started -> paused)
+        _timerStatue = TimerStatus.started;
+        setState(() {
+          _leftControlButtonText = 'Pause';
+          _leftControlButtonIcon = Icons.pause;
+        });
+        break;
+      case TimerStatus.finished: // Restart
+        _restTimerDurationInSeconds = _timerDuration.inSeconds;
+        _startCountdownTimer(seconds: _restTimerDurationInSeconds);
+        _timerStatue = TimerStatus.started;
+        setState(() {
+          _leftControlButtonText = 'Pause';
+          _leftControlButtonIcon = Icons.pause;
+        });
+        break;
+    }
+  }
+
+  // On press the right control button
+  void _rightControlButtonOnPressed() {
+    setState(() {
+      _timerStatue = TimerStatus.set;
+      _leftControlButtonText = 'Start';
+      _leftControlButtonIcon = Icons.play_arrow;
+    });
   }
 
   @override
   void dispose() {
+    //cancel the countdown timer before disposing the screen
     _countdownTimer.cancel();
     super.dispose();
   }
 
   @override
   Widget build(BuildContext context) {
-    bool isMobile = false;
+    bool isMobile =
+        false; // is the App running on a mobile platform (Android or iOS)
+
+    // Check the App running platform (is it Android, iOS, or Web)
     try {
       if ((Platform.isAndroid) || (Platform.isIOS)) {
-        isMobile = true;
+        isMobile = true; // Mobile (Android or iOS)
       } else {
-        isMobile = false;
+        isMobile = false; // Desktop most likely
       }
     } catch (e) {
-      isMobile = false;
+      isMobile = false; // Probably the App is running on the web
     }
-    double circularProgressIndicatorDimensions =
-        isMobile ? MediaQuery.of(context).size.width - 64 : 400;
+    // Set the circular progress indicator dimensions according on the running platform
+    double circularProgressIndicatorDimensions = isMobile
+        ? MediaQuery.of(context).size.width - 64 // Mobile platform
+        : circularProgressIndicatorDimensionsForWeb; // Web/Desktop platform
 
     return Scaffold(
       appBar: AppBar(
@@ -119,10 +208,10 @@ class _MyHomePageState extends State<MyHomePage> with TickerProviderStateMixin {
       body: Column(
         mainAxisAlignment: MainAxisAlignment.start,
         children: <Widget>[
-          _timerStatue == TimerStatus.set
-              ? Padding(
-                  padding: const EdgeInsets.all(32.0),
-                  child: ConstrainedBox(
+          Padding(
+            padding: edgeInsetsPadding,
+            child: _timerStatue == TimerStatus.set
+                ? ConstrainedBox(
                     constraints: BoxConstraints(
                       minWidth: circularProgressIndicatorDimensions,
                       maxWidth: double.infinity,
@@ -131,15 +220,14 @@ class _MyHomePageState extends State<MyHomePage> with TickerProviderStateMixin {
                     ),
                     child: CupertinoTimerPicker(
                         mode: CupertinoTimerPickerMode.hms,
-                        initialTimerDuration: _timerDuration,
+                        initialTimerDuration:
+                            _timerDuration, // Set the initial timer duration
                         onTimerDurationChanged: (Duration duration) {
-                          _timerDuration = duration;
+                          _timerDuration =
+                              duration; // Update the timer duration
                         }),
-                  ),
-                )
-              : Padding(
-                  padding: const EdgeInsets.all(32.0),
-                  child: Stack(children: <Widget>[
+                  )
+                : Stack(children: <Widget>[
                     ConstrainedBox(
                       constraints: BoxConstraints(
                         minWidth: circularProgressIndicatorDimensions,
@@ -149,11 +237,8 @@ class _MyHomePageState extends State<MyHomePage> with TickerProviderStateMixin {
                       ),
                       child: Center(
                         child: Text(
-                          '$_countdownTimerAsString',
-                          style: TextStyle(
-                              color: Colors.amberAccent,
-                              fontWeight: FontWeight.bold,
-                              fontSize: 48),
+                          '$_countdownTimerDurationAsString',
+                          style: countdownDurationTextStyle,
                         ),
                       ),
                     ),
@@ -167,14 +252,14 @@ class _MyHomePageState extends State<MyHomePage> with TickerProviderStateMixin {
                         ),
                         child: CircularProgressIndicator(
                           strokeWidth: 24,
-                          backgroundColor: Colors.blueGrey,
-                          valueColor:
-                              new AlwaysStoppedAnimation<Color>(Colors.red),
+                          backgroundColor:
+                              CircularProgressIndicatorStartupColor,
+                          valueColor: new AlwaysStoppedAnimation<Color>(
+                              CircularProgressIndicatorHeadColor),
                           value: 1 -
                               (_restTimerDurationInSeconds /
                                   _timerDuration.inSeconds) +
                               0.002,
-                          semanticsLabel: 'Linear progress indicator',
                         ),
                       ),
                     ),
@@ -193,93 +278,44 @@ class _MyHomePageState extends State<MyHomePage> with TickerProviderStateMixin {
                                   _timerDuration.inSeconds),
                           valueColor: new AlwaysStoppedAnimation<Color>(
                               (_timerStatue != TimerStatus.finished)
-                                  ? Colors.orange
-                                  : Colors.green),
-                          semanticsLabel: 'Linear progress indicator',
+                                  ? CircularProgressIndicatorValueColor
+                                  : CircularProgressIndicatorDoneColor),
+                          semanticsLabel: 'Counting down',
+                          semanticsValue:
+                              '$_restTimerDurationInSeconds seconds left',
                         ),
                       ),
                     ),
                   ]),
-                ),
+          ),
           Row(
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
               Visibility(
                 visible: (_timerStatue == TimerStatus.finished) ? false : true,
                 child: Padding(
-                  padding: const EdgeInsets.all(12.0),
+                  padding: elevatedButtonPadding,
                   child: ElevatedButton.icon(
                     icon: Icon(_leftControlButtonIcon),
                     label: Text(
                       _leftControlButtonText,
-                      style: TextStyle(color: Colors.white),
+                      style: elevatedButtonTextStyle,
                     ),
-                    onPressed: () {
-                      // Set -> Start -> Pause -> Resume
-                      switch (_timerStatue) {
-                        case TimerStatus.set: // Start Now
-                          _restTimerDurationInSeconds =
-                              _timerDuration.inSeconds;
-                          startCountdownTimer(
-                              seconds: _restTimerDurationInSeconds);
-                          _timerStatue = TimerStatus.started;
-                          setState(() {
-                            _countdownTimerAsString = _durationAsString(
-                                duration: Duration(
-                                    seconds: _restTimerDurationInSeconds));
-                            _leftControlButtonText = 'Pause';
-                            _leftControlButtonIcon = Icons.pause;
-                          });
-                          break;
-                        case TimerStatus.started: // Stop Now
-                          _timerStatue = TimerStatus.paused;
-
-                          setState(() {
-                            _leftControlButtonText = 'Resume';
-                            _leftControlButtonIcon = Icons.play_arrow;
-                          });
-                          break;
-                        case TimerStatus.paused: // Resume Now
-                          // Pause the Timer Now (started -> paused)
-                          _timerStatue = TimerStatus.started;
-                          setState(() {
-                            _leftControlButtonText = 'Pause';
-                            _leftControlButtonIcon = Icons.pause;
-                          });
-                          break;
-                        case TimerStatus.finished: // Restart
-                          _restTimerDurationInSeconds =
-                              _timerDuration.inSeconds;
-                          startCountdownTimer(
-                              seconds: _restTimerDurationInSeconds);
-                          _timerStatue = TimerStatus.started;
-                          setState(() {
-                            _leftControlButtonText = 'Pause';
-                            _leftControlButtonIcon = Icons.pause;
-                          });
-                          break;
-                      }
-                    },
+                    onPressed: _leftControlButtonOnPressed,
                   ),
                 ),
               ),
               Visibility(
                 visible: (_timerStatue == TimerStatus.set) ? false : true,
                 child: Padding(
-                  padding: const EdgeInsets.all(12.0),
+                  padding: elevatedButtonPadding,
                   child: ElevatedButton.icon(
                     icon: Icon(_rightControlButtonIcon),
                     label: Text(
                       _rightControlButtonText, // -> Pause -> Resume
-                      style: TextStyle(color: Colors.white),
+                      style: elevatedButtonTextStyle,
                     ),
-                    onPressed: () {
-                      setState(() {
-                        _timerStatue = TimerStatus.set;
-                        _leftControlButtonText = 'Start';
-                        _leftControlButtonIcon = Icons.play_arrow;
-                      });
-                    },
+                    onPressed: _rightControlButtonOnPressed,
                   ),
                 ),
               )
